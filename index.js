e1fc77f66ad7a588b0d2e0a8507d1dbc4f516e0d
Jehu Frayle <jehufrayle@gmail.com>
2022-06-08 11:44:57 -0700

    First version to deploy

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const API = 'https://randomuser.me/api';
 
-const profilePicture = document.getElementById('profilePicture');
-const profileName = document.getElementById('name');
-const profileLocation = document.getElementById('location');
-const profileFollowers = document.getElementById('followers');
-const profileLikes = document.getElementById('likes');
-const profilePhotos = document.getElementById('photos');
+const [profilePicture, profileName, profileLocation, profileFollowers, profileLikes, profilePhotos] =
+    [document.getElementById('profilePicture'),
+    document.getElementById('name'),
+    document.getElementById('location'),
+    document.getElementById('followers'),
+    document.getElementById('likes'),
+    document.getElementById('photos')];
 
 async function fetchData(api) {
     const data = await (await fetch(api)).json();
@@ -16,11 +17,11 @@ async function fetchData(api) {
 }
 const getRandom = (min, max) => {
     return Math.floor(Math.random() * (max - min) + min);
-  }
+}
 const mediaData = () => {
-    profileFollowers.innerHTML = `${getRandom(10,250)}K`;
+    profileFollowers.innerHTML = `${getRandom(10, 250)}K`;
     profileLikes.innerHTML = `${getRandom(90, 975)}K`;
-    profilePhotos.innerHTML = `${getRandom(100,1501)}`;
+    profilePhotos.innerHTML = `${getRandom(100, 1501)}`;
 }
 const fakeData = () => {
     fetchData(API);
